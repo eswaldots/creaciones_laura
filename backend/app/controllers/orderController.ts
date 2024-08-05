936d65f029b0535647905738e51e46d46938f5e2
eswaldots <aaronvendedor@gmail.com>
2024-08-05 13:32:18 -0400

    Staged all orders

---
+++ backend/app/controllers/orderController.ts
@@ -0,0 +1,30 @@
+import { Request, Response } from "express";
+import { OrderModel } from "../models/orderModel";
+
+export class OrderController {
+	static async getOrders(req: Request, res: Response) {
+		try {
+			const orders = await OrderModel.getOrders();
+
+			return res.status(200).json(orders);
+		}
+
+		catch (error) {
+			console.log(error)
+
+			return res.status(500).json({ message: "Internal server error" });
+		}
+	}
+
+	static async createOrder(req: Request, res: Response) {
+		try {
+			console.log(Date.now())
+			const order = await OrderModel.createOrder(req.body);
+
+			return res.status(201).json(order);
+		} catch (error) {
+			console.log('Fatal error: ', error);
+			return res.status(500).json({ message: "Internal server error" });
+		}
+	}
+}
