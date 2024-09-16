bc4368a6c8a75449a71a146154b3705d241a7fe9
eswaldots <aaronvendedor@gmail.com>
2024-09-16 13:27:39 +0000

    Fixed on another paramas route

--- frontend/src/app/shop/[id]/page.tsx
+++ frontend/src/app/shop/[id]/page.tsx
@@ -1,9 +1,12 @@
-import { Params } from "next/dist/shared/lib/router/utils/route-matcher";
 import Loader from "@/components/ui/loader";
 import { FetchProduct } from "@/lib/data/fetch-product";
 import Modal from "./modal";
 
-export default async function Page({ params }: Params) {
+interface PageProps {
+  params: { id: string };
+}
+
+export default async function Page({ params }: PageProps) {
   const product = await FetchProduct(params.id.toString());
 
   return product ? <Modal item={product} /> : <Loader />;
