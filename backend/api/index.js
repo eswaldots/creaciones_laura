692b6e503a348f6e46cb9e24773309c9c82cf86f
eswaldots <aaronvendedor@gmail.com>
2024-09-17 13:45:24 +0000

    Simplified index.js

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,13 +1,3 @@
 const app = require('../dist/app');
 
-module.exports = (req, res) => {
-  if (typeof app.app === 'function') {
-    return app.app(req, res);
-  } else if (typeof app.handle === 'function') {
-    return app.handle(req, res);
-  } else {
-    console.error('Invalid app object:', app(req, res));
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
-};
 
