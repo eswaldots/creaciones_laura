815203874bad44b0775642f60f7da4c0a9e222d1
Aaron Avila <aaronvendedor@gmail.com>
2024-09-17 07:45:31 -0400

    Delete backend/dist directory

--- backend/dist/routes/ordersRouter.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.OrdersRouter = void 0;
-const express_1 = require("express");
-const orderController_1 = require("../controllers/orderController");
-exports.OrdersRouter = (0, express_1.Router)();
-exports.OrdersRouter.get("/", orderController_1.OrderController.getOrders);
-exports.OrdersRouter.post("/", orderController_1.OrderController.createOrder);
-exports.OrdersRouter.delete("/");
