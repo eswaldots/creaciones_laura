b007b721ec195b10062db707493e95512439b1b3
eswaldots <aaronvendedor@gmail.com>
2024-09-17 13:36:25 +0000

    Console log req and res

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,12 +1,12 @@
 const app = require('../dist/app');
 
 module.exports = (req, res) => {
-  if (typeof app === 'function') {
+  if (typeof app.app === 'function') {
     return app(req, res);
   } else if (typeof app.handle === 'function') {
     return app.handle(req, res);
   } else {
-    console.error('Invalid app object:', app);
+    console.error('Invalid app object:', app(req, res));
     res.status(500).json({ error: 'Internal Server Error' });
   }
 };
